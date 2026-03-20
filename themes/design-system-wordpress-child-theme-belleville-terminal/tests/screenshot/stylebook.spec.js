3e6f2a7a08211eff16b8427a3e6876bcb102aca5
Michael Haswell <Michael.Haswell@gov.bc.ca>
2026-03-20 15:56:39 -0700

    Remove engine specification in theme package.json (this is controlled by monorepo package.json) Add excluded blocks from stylebook regression tests

--- themes/design-system-wordpress-child-theme-belleville-terminal/tests/screenshot/stylebook.spec.js
+++ themes/design-system-wordpress-child-theme-belleville-terminal/tests/screenshot/stylebook.spec.js
@@ -14,6 +14,11 @@ test.describe( 'style book', () => {
             'post-template',
             'query-total',
             'spacer',
+            'rss',
+            'video',
+            'calendar',
+            'latest-comments',
+            'archives',
         ];
 
         await admin.visitAdminPage(
